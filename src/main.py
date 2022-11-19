52cdb579b044dffdbd037ad3bd28cccdeece0832
KipzonderKop101 <lodewijkboon@icloud.com>
2022-11-19 16:03:12 +0100

    Added reload command, bot owner only

--- src/main.py
+++ src/main.py
@@ -20,4 +20,25 @@ directories = ['General', 'Moderation']
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
 
+@client.command()
+@commands.is_owner()
+async def reload(ctx):
+    not_loaded = []
+    for directory in directories:
+        for file in os.listdir(os.path.dirname(__file__) + f"/cogs/{directory}"):
+            if file.endswith(".py"):
+                try:
+                    client.unload_extension(f"cogs.{directory}.{file[:-3]}")
+                    client.load_extension(f"cogs.{directory}.{file[:-3]}")
+                except Exception as e:
+                    not_loaded.append(f"- **{file}** | `{e}`")
+    await ctx.respond(f"Files reloaded! Errors:" + "\n".join(not_loaded))
+
+
+for directory in directories:
+    for file in os.listdir(os.path.dirname(__file__) + f"/cogs/{directory}"):
+        if file.endswith(".py"):
+            client.load_extension(f"cogs.{directory}.{file[:-3]}")
+            print(f"Loaded {file}")
+
 client.run(TOKEN)
