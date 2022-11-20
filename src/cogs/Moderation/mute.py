abb83c654d9210ac7642d03ac0514e5da8577972
KipzonderKop101 <lodewijkboon@icloud.com>
2022-11-20 14:12:13 +0100

    Muted role now gives correct permissions

--- src/cogs/Moderation/mute.py
+++ src/cogs/Moderation/mute.py
@@ -8,14 +8,29 @@ class Mute(commands.Cog):
 
     @discord.slash_command(description="Mute a member")
     async def mute(self, ctx, member: Option(discord.Member, 'The member to mute', required=True), reason: Option(str, 'The reason for the mute', required=False)):
+        await ctx.defer()
+        overwrite = discord.PermissionOverwrite()
+        overwrite.send_messages = False
+        overwrite.add_reactions = False
+        overwrite.create_private_threads = False
+        overwrite.create_public_threads = False
+        overwrite.send_messages_in_threads = False
         # Check if there is a muted role and if not, create one
         if not discord.utils.get(ctx.guild.roles, name="Muted") and not discord.utils.get(ctx.guild.roles, name="muted"):
-            await ctx.guild.create_role(name="Muted", reason="Muted role not found")
+            muted_role = await ctx.guild.create_role(name="Muted", reason="Muted role not found")
+            for channel in ctx.guild.channels:
+                await channel.set_permissions(muted_role, overwrite=overwrite)
             mutedRole = discord.utils.get(ctx.guild.roles, name="Muted")
         else:
             mutedRole = discord.utils.get(ctx.guild.roles, name="Muted")
-            if not mutedRole:
+            if mutedRole:
+                for channel in ctx.guild.channels:  
+                    await channel.set_permissions(mutedRole, overwrite=overwrite)
+            else:
                 mutedRole = discord.utils.get(ctx.guild.roles, name="muted")
+                for channel in ctx.guild.channels:
+                    await channel.set_permissions(mutedRole, overwrite=overwrite)
+
         # Check if the member has the muted role
         if mutedRole in member.roles:
             await ctx.respond(f"{member.mention} is already muted!")
@@ -23,10 +38,10 @@ class Mute(commands.Cog):
             # Check if there is a reason
             if reason == None:
                 await member.add_roles(mutedRole, reason="No reason provided")
-                await ctx.respond(f"{member.mention} has been muted!")
+                await ctx.respond(f"{member.mention} has been muted!", ephemeral=True)
             else:
                 await member.add_roles(mutedRole, reason=reason)
-                await ctx.respond(f"{member.mention} has been muted for {reason}!")
+                await ctx.respond(f"{member.mention} has been muted for {reason}!", ephemeral=True)
 
 def setup(client):
     client.add_cog(Mute(client))
