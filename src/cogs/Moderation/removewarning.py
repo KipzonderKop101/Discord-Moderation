43d7e365a16a07b9ebccdbf5070e152c7b6f9956
KipzonderKop101 <lodewijkboon@icloud.com>
2022-11-20 12:52:37 +0100

    Command to remove warning

---
+++ src/cogs/Moderation/removewarning.py
@@ -0,0 +1,43 @@
+import discord
+from discord import Option
+from discord.ext import commands
+from utils.warningstore import WarningStore
+
+class RemoveWarning(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+    
+    @discord.slash_command(description="Remove a warning from a member")
+    async def removewarning(self, ctx, member: Option(discord.Member, 'The member to remove the warning from', required=True), warning_name: Option(str, 'The warning to remove (by name)', required=False), warning_index: Option(str, 'The warning to remove (by index)', required=False)):
+
+        # Create instance of WarningStore
+        warning_store = WarningStore()
+        
+        # Check if there is an entry, and if not, create one
+        warning_store.create_entry(member.id)
+        
+        # Get the warnings for this member
+        warnings = warning_store.get_warnings(member.id)
+        
+        # Check if the member has any warnings, if so, remove the warning
+        if len(warnings) > 0:
+            if warning_name == None and warning_index == None:
+                await ctx.respond('You must specify a warning to remove', ephemeral=True)
+            elif warning_name != None and warning_index == None:
+                warning_store.remove_warning(member.id, warning_name)
+                await ctx.respond(f'Removed warnings "{warning_name}" from {member.mention}')
+            elif warning_index != None and warning_name == None:
+                warning_store.remove_warning_by_index(member.id, int(warning_index)) 
+                await ctx.respond(f'Removed warning at index {warning_index} from {member.mention}')
+            elif warning_name != None and warning_index != None:
+                warning_by_index = warning_store.get_warning_by_index(member.id, int(warning_index))
+                if warning_by_index == warning_name:
+                    warning_store.remove_warning_by_index(member.id, int(warning_index))
+                else:
+                    await ctx.respond('The warning name and index do not match', ephemeral=True)
+        else:
+            await ctx.respond('This member has no warnings', ephemeral=True)
+
+
+def setup(client):
+    client.add_cog(RemoveWarning(client))
