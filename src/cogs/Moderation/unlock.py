040eee14b859e384b52ae90f316943f8a0d61aa3
KipzonderKop101 <lodewijkboon@icloud.com>
2022-11-22 11:39:47 +0100

    Also logs

--- src/cogs/Moderation/unlock.py
+++ src/cogs/Moderation/unlock.py
@@ -1,14 +1,19 @@
 import discord
 from discord import Option
 from discord.ext import commands
+from utils.modlog import ModLog
 
 class Unlock(commands.Cog):
     def __init__(self, client):
         self.client = client
     
     @discord.slash_command(description="Unlock a channel")
-    async def unlock(self, ctx, channel: Option(discord.abc.GuildChannel, 'Channel to unlock, default is current', required=False)):
+    async def unlock(self, ctx, channel: Option(discord.abc.GuildChannel, 'Channel to unlock, default is current', required=False), reason: Option(str, 'Reason to unlock this channel', required=False)):
         await ctx.defer()
+        if reason is None: reason = "No reason provided"
+
+        log = ModLog()
+
         overwrite = discord.PermissionOverwrite()
         overwrite.send_messages = True
         overwrite.add_reactions = True
@@ -18,6 +23,8 @@ class Unlock(commands.Cog):
         if channel is None: channel = ctx.channel
         await channel.set_permissions(ctx.guild.default_role, overwrite=overwrite)
         await ctx.respond(f"Unlocked {channel.mention}", ephemeral=True)
+
+        await log.log(ctx, "Set slowmode", reason)
 
 def setup(client):
     client.add_cog(Unlock(client))
